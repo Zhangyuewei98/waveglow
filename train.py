0beaef77cb78ae08afb45c2c8a5f519cbe83f9cd
begeekmyfriend <begeekmyfriend@gmail.com>
2020-03-30 14:44:09 +0800

    Longer segment length
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- train.py
+++ train.py
@@ -106,10 +106,10 @@ def train(num_gpus, rank, group_name, output_directory, epochs,
     # =====START: ADDED FOR DISTRIBUTED======
     train_sampler = DistributedSampler(trainset) if num_gpus > 1 else None
     # =====END:   ADDED FOR DISTRIBUTED======
-    train_loader = DataLoader(trainset, num_workers=8, shuffle=False,
+    train_loader = DataLoader(trainset, num_workers=8, shuffle=True,
                               sampler=train_sampler,
                               batch_size=batch_size,
-                              pin_memory=False,
+                              pin_memory=True,
                               drop_last=True)
 
     # Get shared output_directory ready
